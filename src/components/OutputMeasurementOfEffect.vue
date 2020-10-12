d8486443aca94ed2c4dacaf9b566d7f152a3f099
Connor Forbes <connorforbes25@gmail.com>
2020-10-12 17:08:27 +1000

    OUTPUT: Measurement of Effect

---
+++ src/components/OutputMeasurementOfEffect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p v-if="data.toolUsed">
+      {{ capitalize(data.toolUsed) }} was used to calculate the treatment
+      effect.
+    </p>
+    <p v-if="data.dichotomousOutcomes">
+      We used {{ formatSelectMulti(data.dichotomousOutcomes) }} for dichotomous
+      outcomes.
+    </p>
+    <p>
+      We undertook meta-analyses when {{ data.metaAnalysisThreshold }} studies
+      or comparisons reported the same outcome.
+    </p>
+    <p>
+      We used a {{ data.metaAnalysisModelUsed.toLowerCase() }} effects model.
+    </p>
+  </div>
+</template>
+
+<script>
+import OutputMixin from "../mixins/OutputMixin.js";
+export default {
+  name: "OutputMeasurementOfEffect",
+  mixins: [OutputMixin],
+  props: {
+    data: Object
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
