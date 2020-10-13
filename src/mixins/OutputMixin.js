f00e820ca9fc60a0e92dcfaf6ec5baab6f90f7f6
Connor Forbes <connorforbes25@gmail.com>
2020-10-13 12:20:42 +1000

    BUGFIX: Case for only 1 in array

--- src/mixins/OutputMixin.js
+++ src/mixins/OutputMixin.js
@@ -5,10 +5,14 @@ export default {
       return s.charAt(0).toUpperCase() + s.slice(1);
     },
     joinArrayWithAnd(arr) {
-      return arr.slice(0, -1).join(", ") + " and " + arr.slice(-1);
+      if (arr.length > 1)
+        return arr.slice(0, -1).join(", ") + " and " + arr.slice(-1);
+      else return arr[0];
     },
     joinArrayWithOr(arr) {
-      return arr.slice(0, -1).join(", ") + " or " + arr.slice(-1);
+      if (arr.length > 1)
+        return arr.slice(0, -1).join(", ") + " or " + arr.slice(-1);
+      else return arr[0];
     },
     formatSelectMulti(arr) {
       // Get only the labels the array of objects
