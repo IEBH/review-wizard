3f05852b503ac7fc5b4133a4d37a3a5fc2049ac5
Connor Forbes <connorforbes25@gmail.com>
2020-12-07 15:24:39 +1000

    Extraction alternative outputs

--- src/components/OutputDataExtraction.vue
+++ src/components/OutputDataExtraction.vue
@@ -1,21 +1,45 @@
 <template>
   <div>
     <p>
-      We used a data extraction form for study characteristics and outcome data,
-      which was piloted on
-      {{ data.numberOfStudies ? data.numberOfStudies : "BLANK" }} studies in the
-      review.
+      {{
+        selectRandom([
+          "We used a data extraction form for study characteristics and outcome data, which was piloted on "
+            .concat(data.numberOfStudies ? data.numberOfStudies : "BLANK")
+            .concat(" studies in the review."),
+          "A standardised form (initially piloted on "
+            .concat(data.numberOfStudies ? data.numberOfStudies : "BLANK")
+            .concat(" incluced studies) was used for data extraction."),
+          "Study characteristics and outcomes data from each study were recorded in a data extraction form, which was initially piloted on "
+            .concat(data.numberOfStudies ? data.numberOfStudies : "BLANK")
+            .concat(" studies.")
+        ])
+      }}
     </p>
     <p>
-      {{ data.numberOfExtractors ? data.numberOfExtractors : "BLANK" }} study
-      authors extracted the following data from included studies:
+      {{
+        selectRandom([
+          ""
+            .concat(data.numberOfExtractors ? data.numberOfExtractors : "BLANK")
+            .concat(
+              " study authors extracted the following data from included studies:"
+            ),
+          "Data extraction was conducted by "
+            .concat(data.numberOfExtractors ? data.numberOfExtractors : "BLANK")
+            .concat(" authors. ")
+            .concat(
+              "The following data for study characteristics and outcomes were extracted from each included study:"
+            )
+        ])
+      }}
     </p>
   </div>
 </template>
 
 <script>
+import OutputMixin from "../mixins/OutputMixin.js";
 export default {
   name: "OutputDataExtraction",
+  mixins: [OutputMixin],
   props: {
     data: Object
   }
