ffb382c16a1a3fdd0a4e38f63a2d4d206da23793
Connor Forbes <connorforbes25@gmail.com>
2021-04-06 12:05:45 +1000

    style: Undefined cases for citation

--- src/components/TheArticleCitation.vue
+++ src/components/TheArticleCitation.vue
@@ -3,7 +3,9 @@
     {{
       `(${formatAuthors(authors)
         .filter(author => author)
-        .join(" ")} ${year}. ${title}.)`
+        .join(" ")} ${year === 0 ? "No year specified" : year}. ${
+        title ? title : "No title specified"
+      }.)`
     }}
   </span>
 </template>
@@ -19,8 +21,11 @@ export default {
     formatAuthors(arr) {
       return arr.map(author => {
         author = author.split(" ");
-        if (author[1]) return `${author[1]} ${author[0].charAt(0)}.,`;
-        else if (author[0]) return author[0];
+        if (author[1] && author[0])
+          return `${author[1]} ${author[0].charAt(0)}.,`;
+        else if (author[1]) return `${author[1]},`;
+        else if (author[0]) return `${author[0]},`;
+        else return "No author specified.,";
       });
     }
   }
