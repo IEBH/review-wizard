9e32af9fe09cfa049bb300832f745a9069ef2a63
connorf25 <connorforbes25@gmail.com>
2021-08-03 18:51:04 +1000

    refactor: Initialize deepstream entirely in store

--- src/components/ProjectEdit.vue
+++ src/components/ProjectEdit.vue
@@ -3,8 +3,6 @@
 </template>
 
 <script>
-import { DeepstreamClient } from "@deepstream/client";
-
 export default {
 	name: "ProjectEdit",
 	created() {
@@ -24,36 +22,10 @@ export default {
 		async refresh() {
 			var projectId =
 				this.$route.query["project-id"] || this.$route.params.projectId;
-			if (this.prevId !== projectId) {
-				console.log("Loading Project:", `project/${projectId}`);
-				if (projectId) {
-					// Update store with project ID
-					this.$store.commit("setProjectId", projectId);
-					// Inizialize deepstream client
-					var client = new DeepstreamClient("localhost:6020");
-					// Login to deepstream
-					client.login();
-					// Add project record
-					var projectRecord = client.record.getRecord(`project/${projectId}`);
-					// Subscribe to project metadata
-					var projectMetadata = await new Promise(resolve => {
-						projectRecord.subscribe("metadata", metadata => {
-							resolve(metadata);
-						});
-					});
-					if (projectMetadata) {
-						// Add methods record
-						var methodsRecord = client.record.getRecord(
-							`methods/${projectMetadata.methods}`
-						);
-						// Set methods record to store
-						this.$store.commit("setMethodsRecord", methodsRecord);
-					} else {
-						console.error("Invalid project-id");
-					}
-				} else {
-					console.error("No project id specified");
-				}
+			if (this.prevId != projectId) {
+				console.log("Initializing deepstream");
+				// Initialize deepstream
+				this.$store.dispatch("initialize", projectId);
 			}
 		}
 	}
--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,60 @@ import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
 
+import { DeepstreamClient } from "@deepstream/client";
+
 const storeData = {
 	state: {
 		projectId: null,
+		client: new DeepstreamClient("localhost:6020"),
+		projectRecord: null,
 		methodsRecord: null
 	},
 	mutations: {
 		setProjectId(state, id) {
 			state.projectId = id;
 		},
+		setClient(state, client) {
+			state.client = client;
+		},
+		setProjectRecord(state, record) {
+			state.projectRecord = record;
+		},
 		setMethodsRecord(state, record) {
 			state.methodsRecord = record;
+		}
+	},
+	actions: {
+		initialize: async function({ commit, state }, projectId) {
+			console.log("Loading Project:", `project/${projectId}`);
+			if (projectId) {
+				// Update store with project ID
+				commit("setProjectId", projectId);
+				// Login to deepstream
+				state.client.login();
+				// Set project record
+				commit(
+					"setProjectRecord",
+					state.client.record.getRecord(`project/${projectId}`)
+				);
+				// Subscribe to project metadata
+				var projectMetadata = await new Promise(resolve => {
+					state.projectRecord.subscribe("metadata", metadata => {
+						resolve(metadata);
+					});
+				});
+				if (projectMetadata) {
+					// Set methods record
+					commit(
+						"setMethodsRecord",
+						state.client.record.getRecord(`methods/${projectMetadata.methods}`)
+					);
+				} else {
+					console.error("Invalid project-id");
+				}
+			} else {
+				console.error("No project id specified");
+			}
 		}
 	}
 };
