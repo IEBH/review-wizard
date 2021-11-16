9cce0d4416fc077122f7b7aef6ea70b36680a8b7
Connor Forbes <connorforbes25@gmail.com>
2021-11-17 09:32:17 +1000

    feat: Update dateModified when changing method data

--- src/mixins/DeepstreamMixin.js
+++ src/mixins/DeepstreamMixin.js
@@ -109,6 +109,9 @@ export default key => ({
 	computed: {
 		methodsRecord() {
 			return this.$store.state.methodsRecord;
+		},
+		projectRecord() {
+			return this.$store.state.projectRecord;
 		}
 	},
 	mounted() {
@@ -131,6 +134,7 @@ export default key => ({
 		updateField(field, value) {
 			this.$set(this[key], field, value);
 			this.methodsRecord.set(key, this[key]);
+			this.projectRecord.set("metadata.dateModified", Date.now());
 		}
 	}
 });
