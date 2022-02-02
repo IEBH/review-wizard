d46a6b16b11e5bf1851f0946e825cc40b4a615e0
Connor Forbes <connorforbes25@gmail.com>
2022-02-02 13:22:17 +1000

    refactor: Globally register all input components

--- src/main.js
+++ src/main.js
@@ -20,6 +20,45 @@ Vue.use(ToastService);
 
 Vue.config.productionTip = false;
 
+// Register all Input Components Globally {{{
+import upperFirst from "lodash/upperFirst";
+import camelCase from "lodash/camelCase";
+
+const requireComponent = require.context(
+	// The relative path of the components folder
+	"./components",
+	// Whether or not to look in subfolders
+	false,
+	// The regular expression used to match base component filenames
+	/Input[A-Z]\w+\.(vue|js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+	// Get component config
+	const componentConfig = requireComponent(fileName);
+
+	// Get PascalCase name of component
+	const componentName = upperFirst(
+		camelCase(
+			// Gets the file name regardless of folder depth
+			fileName
+				.split("/")
+				.pop()
+				.replace(/\.\w+$/, "")
+		)
+	);
+
+	// Register component globally
+	Vue.component(
+		componentName,
+		// Look for the component options on `.default`, which will
+		// exist if the component was exported with `export default`,
+		// otherwise fall back to module's root.
+		componentConfig.default || componentConfig
+	);
+});
+// }}}
+
 (async () => {
 	const router = await initializeRouter();
 	new Vue({
