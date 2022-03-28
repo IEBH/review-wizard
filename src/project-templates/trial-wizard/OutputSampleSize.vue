b96e7d3799c212eec6409dbcdf136c7557fd437d
Justin-Clarc <31459238+Justin-Clarc@users.noreply.github.com>
2022-03-28 16:33:48 +1000

    Added sample size sheet

--- src/project-templates/trial-wizard/OutputSampleSize.vue
+++ src/project-templates/trial-wizard/OutputSampleSize.vue
@@ -1,13 +1,38 @@
 <template>
 	<div>
-		<p></p>
+		<p>
+
+			{{
+					selectRandom([
+						"We used Power and Sample Size (PS) to conduct the calculations ",
+						"We did the calculations using used Power and Sample Size (PS) from Vanderbilt University "
+
+					])
+			}}	
+			{{ data.calculation }}.
+
+		</p>
+
+				<p>
+
+			{{
+					selectRandom([
+						"We used Sealed envelope to conduct the calculations ",
+						"We did the calculations using Sealed Envelope used Power and Sample Size (PS) from Vanderbilt University "
+
+					])
+			}}	
+			{{ data.nonInf }}.
+
+		</p>
+
 	</div>
 </template>
 
 <script>
 import OutputMixin from "@/mixins/OutputMixin.js";
 export default {
-	name: "OutputInterventions",
+	name: "OutputSampleSize",
 	mixins: [OutputMixin],
 	props: {
 		data: Object
--- src/project-templates/trial-wizard/ViewMethodSampleSize.vue
+++ src/project-templates/trial-wizard/ViewMethodSampleSize.vue
@@ -1,7 +1,23 @@
 <template>
 	<div>
 		<h1>Sample Size</h1>
-		Under construction
+
+		<i>Estimated number of participants needed to achieve study objectives and how it was determined, including clinical and statistical assumptions supporting any sample size calculations</i>
+
+		<InputTextSingleLine
+			question="To calculate the sample size we suggest using Power and Sample Size (PS) from Vanderbilt University and copying the text it provides: https://vbiostatps.app.vumc.org/ps/"
+			placeholder="Sample size calculation"
+			:value="sampleSize.calculation"
+			@input="updateField('calculation', $event)"
+		/>
+
+		<InputTextSingleLine
+			question="For non-inferiority trials use Sealed Envelope: https://www.sealedenvelope.com/"
+			placeholder="Sample size calculation"
+			:value="sampleSize.nonInf"
+			@input="updateField('nonInf', $event)"
+		/>
+
 		<BasePreviewOutput :component="outputComponent" :data="sampleSize" />
 	</div>
 </template>
