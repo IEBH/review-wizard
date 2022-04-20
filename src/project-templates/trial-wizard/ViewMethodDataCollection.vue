8ae33474210043a537410126e777a8d06c8a5bf3
Connor Forbes <connorforbes25@gmail.com>
2022-04-20 12:04:45 +1000

    refactor(data-collection): cleared existing

--- src/project-templates/trial-wizard/ViewMethodDataCollection.vue
+++ src/project-templates/trial-wizard/ViewMethodDataCollection.vue
@@ -2,52 +2,7 @@
 	<div>
 		<h1>Data Collection, Management and Analysis</h1>
 
-		<i
-			>Data collection methods: Plans for assessment and collection of outcome,
-			baseline, and other trial data, including any related processes to promote
-			data quality (eg, duplicate measurements, training of assessors) and a
-			description of study instruments (eg, questionnaires, laboratory tests)
-			along with their reliability and validity, if known. Reference to where
-			data collection forms can be found, if not in the protocol. Plans to
-			promote participant retention and complete follow-up, including list of
-			any outcome data to be collected for participants who discontinue or
-			deviate from intervention protocols.</i
-		>
 
-		<InputTableCustom
-			question="How was data collected?"
-			:columns="interventionColumns"
-			:value="dataCollection.sequenceGen"
-			@input="updateField('sequenceGen', $event)"
-		/>
-
-		<i
-			>Data management: Plans for data entry, coding, security, and storage,
-			including any related processes to promote data quality (eg, double data
-			entry; range checks for data values). Reference to where details of data
-			management procedures can be found, if not in the protocol.</i
-		>
-
-		<InputTableCustom
-			question="How was the data managed?"
-			:columns="interventionColumns"
-			:value="dataCollection.sequenceGen"
-			@input="updateField('sequenceGen', $event)"
-		/>
-
-		<i
-			>Statistical methods: Statistical methods for analysing primary and
-			secondary outcomes. Reference to where other details of the statistical
-			analysis plan can be found, if not in the protocol. Methods for any
-			additional analyses (eg, subgroup and adjusted analyses).</i
-		>
-
-		<InputTableCustom
-			question="What statistical methods were used to analyse the outcomes?"
-			:columns="interventionColumns"
-			:value="dataCollection.sequenceGen"
-			@input="updateField('sequenceGen', $event)"
-		/>
 
 		<BasePreviewOutput :component="outputComponent" :data="dataCollection" />
 	</div>
