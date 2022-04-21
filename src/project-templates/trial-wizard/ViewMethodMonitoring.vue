a4ba972614c5d65567b0630aa1ec73183661a690
Connor Forbes <connorforbes25@gmail.com>
2022-04-21 11:11:31 +1000

    feat(monitoring): Input complete

--- src/project-templates/trial-wizard/ViewMethodMonitoring.vue
+++ src/project-templates/trial-wizard/ViewMethodMonitoring.vue
@@ -29,60 +29,103 @@
 
 		<InputSelectYesNo
 			question="Will a data monitoring committee be set up?"
-			:value="monitoring.committee"
-			@input="updateField('committee', $event)"
+			:value="monitoring.isCommittee"
+			@input="updateField('isCommittee', $event)"
 		/>
 
-		<Accordion
-			v-if="monitoring.optionalDetail"
-			style="margin-top: 50px; border: 1px solid black;"
-		>
-			<AccordionTab header="Optional Details" :active="true">
-				<h3></h3>
-				<InputTextSingleLine
-					question="Provide details of the role the committee had"
-					placeholder="The committee had oversight of whether the trial could be ended early due to an unacceptable number of adverse events"
-					:value="monitoring.committeeRole"
-					@input="updateField('committeeRole', $event)"
-				/>
-				/>
-				<InputTextSingleLine
-					question="Provide details of the reporting structure of the committee"
-					placeholder="The committee reported directly to the ethics board of the primary trial hospital"
-					:value="monitoring.committeeReporting"
-					@input="updateField('committeeReporting', $event)"
-				/>
-				<InputTextSingleLine
-					question="Provide details of the composition of the committee"
-					placeholder="The committee was comprised of the director of the hospital, the head of the ethics committee and an external adverse effects consultant"
-					:value="monitoring.committeeComposition"
-					@input="updateField('committeeComposition', $event)"
-				/>
-			</AccordionTab>
-		</Accordion>
+		<div v-if="monitoring.isCommittee">
+			<InputTextSingleLine
+				question="Provide details of the role the committee had"
+				placeholder="the committee had oversight of whether the trial could be ended early due to an unacceptable number of adverse events"
+				:value="monitoring.committeeDetails"
+				@input="updateField('committeeDetails', $event)"
+			/>
+			<InputTextSingleLine
+				question="Provide details of the reporting structure of the committee"
+				placeholder="the committee reported directly to the ethics board of the primary trial hospital"
+				:value="monitoring.committeeReporting"
+				@input="updateField('committeeReporting', $event)"
+			/>
+			<InputTextSingleLine
+				question="Provide details of the composition of the committee"
+				placeholder="the committee was comprised of the director of the hospital, the head of the ethics committee and an external adverse effects consultant"
+				:value="monitoring.committeeComposition"
+				@input="updateField('committeeComposition', $event)"
+			/>
+			<InputTextSingleLine
+				question="Further details about the committee can be found at the following link"
+				placeholder=""
+				:value="monitoring.committeeLink"
+				@input="updateField('committeeLink', $event)"
+			/>
+		</div>
 
-		<InputTextSingleLine
-			question="List the reasons why a committee was not set up for this study"
-			placeholder="No committee was set up for this trial as the participant numbers were so small"
-			:value="monitoring.noCommittee"
-			@input="updateField('noCommittee', $event)"
-		/>
+		<div v-if="!monitoring.isCommittee">
+			<InputTextSingleLine
+				question="List the reason(s) why a committee was not set up for this study"
+				placeholder="no committee was set up for this trial as the participant numbers were so small"
+				:value="monitoring.committeeNotSetUpReasons"
+				@input="updateField('committeeNotSetUpReasons', $event)"
+			/>
+		</div>
+
 		<InputSelectYesNo
 			question="Will an interim analysis be conducted?"
-			:value="monitoring.interimAnalysis"
-			@input="updateField('interimAnalysis', $event)"
+			:value="monitoring.isInterimAnalysis"
+			@input="updateField('isInterimAnalysis', $event)"
 		/>
 
+		<div v-if="monitoring.isInterimAnalysis">
+			<InputTextSingleLine
+				question="What time-points was the interim analysis conducted at"
+				placeholder="the beginning and end of the trial"
+				:value="monitoring.interimAnalysisTimePoints"
+				@input="updateField('interimAnalysisTimePoints', $event)"
+			/>
+			<InputSelectMulti
+				question="Interim analysis results were made available to:"
+				:options="interimResultsAvailableOptions"
+				:value="monitoring.interimResultsAvailable"
+				@input="updateField('interimResultsAvailable', $event)"
+			/>
+			<InputTextSingleLine
+				question="The study could be stopped if..."
+				placeholder="more than 10% of participants experienced severe adverse reactions"
+				:value="monitoring.earlyStop"
+				@input="updateField('earlyStop', $event)"
+			/>
+			<InputSelectMulti
+				question="Who made the final decision on study termination?"
+				:options="finalDecisionOnTerminationOptions"
+				:value="monitoring.finalDecisionOnTermination"
+				@input="updateField('finalDecisionOnTermination', $event)"
+			/>
+		</div>
+
+		<div v-if="!monitoring.isInterimAnalysis">
+			<InputTextSingleLine
+				question="List the reason(s) why an interim analysis was not planned"
+				placeholder="No committee was set up for this trial as the participant numbers were so small"
+				:value="monitoring.interimNotConductedReasons"
+				@input="updateField('interimNotConductedReasons', $event)"
+			/>
+		</div>
+
+		<i
+			>Provide plans for collecting, assessing, reporting, and managing
+			solicited and spontaneously reported adverse events and other unintended
+			effects of trial interventions or trial conduct</i
+		>
 		<InputTextSingleLine
 			question="How were adverse effects monitored?"
-			placeholder="Provide plans for collecting, assessing, reporting, and managing solicited and spontaneously reported adverse events and other unintended effects of trial interventions or trial conduct."
-			:value="monitoring.adverseEffects"
-			@input="updateField('adverseEffects', $event)"
+			placeholder="by weekly check-ups with a GP to monitor blood-pressure and general health"
+			:value="monitoring.adverseEventsMonitored"
+			@input="updateField('adverseEventsMonitored', $event)"
 		/>
 
 		<InputTextSingleLine
-			question="Was a trial audit planned?"
-			placeholder="Provide frequency and procedures for auditing trial conduct, if any, and whether the process will be independent from investigators and the sponsor."
+			question="Provide frequency and procedures for auditing trial conduct, if any, and whether the process will be independent from investigators and the sponsor"
+			placeholder="weekly through an independent consultant"
 			:value="monitoring.auditPlan"
 			@input="updateField('auditPlan', $event)"
 		/>
@@ -99,7 +142,9 @@ export default {
 	mixins: [deepstreamMixin("monitoring")],
 	data() {
 		return {
-			outputComponent: OutputMonitoring
+			outputComponent: OutputMonitoring,
+			interimResultsAvailableOptions: [{ label: "DMC" }],
+			finalDecisionOnTerminationOptions: [{ label: "study investigators" }]
 		};
 	}
 };
