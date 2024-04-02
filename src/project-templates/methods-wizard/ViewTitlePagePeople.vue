87beb763d8ae0846f04f607b37f74f41c54b6dbe
Octian <115122605+Octian@users.noreply.github.com>
2024-04-02 16:19:00 +1000

    Update ViewTitlePagePeople.vue

--- src/project-templates/methods-wizard/ViewTitlePagePeople.vue
+++ src/project-templates/methods-wizard/ViewTitlePagePeople.vue
@@ -4,13 +4,13 @@
 
 		<InputTextSingleLineMulti
 			question="Who are the authors in the study?"
-			v-model="authors"
+			v-model="$tera.state.author"
 			placeholder="e.g. Justin Clark"
 		/>
 
 		<InputTextSingleLineMulti
 			question="Who else helped with the study? (Acknowledgements)"
-			v-model="acknowledgements"
+			v-model="$tera.state.acknowledgements"
 			placeholder="e.g. Justin Clark"
 		/>
 	</div>
@@ -25,24 +25,15 @@ export default {
 	components: {
 		InputTextSingleLineMulti
 	},
-	computed: {
-		authors() {
-			if (this.$tera.state.author == undefined) {
-				this.$tera.setProjectStateDefaults(
-					"author",
-					defaults.titlepage.authors
-				);
-			}
-			return this.$tera.state.author;
-		},
-		acknowledgements() {
-			if (this.$tera.state.acknowledgements == undefined) {
-				this.$tera.setProjectStateDefaults(
-					"acknowledgements",
-					defaults.titlepage.acknowledgements
-				);
-			}
-			return this.$tera.state.acknowledgements;
+	mounted() {
+		if (this.$tera.state.author == undefined) {
+			this.$tera.setProjectStateDefaults("author", defaults.titlepage.authors);
+		}
+		if (this.$tera.state.acknowledgements == undefined) {
+			this.$tera.setProjectStateDefaults(
+				"acknowledgements",
+				defaults.titlepage.acknowledgements
+			);
 		}
 	}
 };
