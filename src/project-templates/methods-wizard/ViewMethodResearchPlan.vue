3d73b1b604cf6a75eb70d864664589a1c01ef00c
Octian <115122605+Octian@users.noreply.github.com>
2024-04-10 10:19:40 +1000

    Update ViewMethodResearchPlan.vue

--- src/project-templates/methods-wizard/ViewMethodResearchPlan.vue
+++ src/project-templates/methods-wizard/ViewMethodResearchPlan.vue
@@ -35,8 +35,12 @@ export default {
 			if (this.$tera.state.planTable == undefined) {
 				let table = this.checkAuthorSelection(defaults.researchplan.planTable);
 				this.$tera.setProjectStateDefaults("planTable", table);
-				return table;
+				console.log(
+					"undefined: table:" + JSON.stringify(this.$tera.state.planTable)
+				);
+				return this.$tera.state.planTable;
 			} else {
+				console.log("table:" + JSON.stringify(this.$tera.state.planTable));
 				return this.checkAuthorSelection(this.$tera.state.planTable);
 			}
 		},
