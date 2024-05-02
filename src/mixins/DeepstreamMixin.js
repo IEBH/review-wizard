e4f21a7ceb39b9ef9440179f8fafb8f007684111
Matt Carter <m@ttcarter.com>
2024-05-02 12:28:29 +1000

    refactor: Removed state default handling for projects on load from mixin

--- src/mixins/DeepstreamMixin.js
+++ src/mixins/DeepstreamMixin.js
@@ -1,18 +1,4 @@
-import projectTemplateImport from "@/helpers/projectTemplateImport.js";
-
-const getDefaultData = async key => {
-	const { data } = await projectTemplateImport();
-	return data[key];
-};
-let dataIsReady;
-
 export default key => ({
-	data() {
-		return {
-			[key]: {},
-			dataReady: new Promise(resolve => (dataIsReady = resolve))
-		};
-	},
 	computed: {
 		methodsRecord() {
 			return this.$store.state.methodsRecord;
@@ -21,28 +7,9 @@ export default key => ({
 			return this.$store.state.projectRecord;
 		}
 	},
-	async mounted() {
-		this[key] = await getDefaultData(key);
-		// Resolve promise marking that data is ready
-		dataIsReady();
-		if (this.methodsRecord) {
-			var existing = this.methodsRecord.get(key);
-			if (existing) {
-				this[key] = existing;
-			}
-			this.methodsRecord.subscribe(key, picot => {
-				this[key] = picot;
-			});
-		}
-	},
-	destroyed() {
-		if (this.methodsRecord) {
-			this.methodsRecord.unsubscribe(key);
-		}
-	},
 	methods: {
 		updateField(field, value) {
-			console.log('fieldsssss',field,value)
+			console.info('@depreciated updateField(', {field, value}, ')');
 			this.$set(this[key], field, value);
 			this.methodsRecord.set(key, this[key]);
 			this.projectRecord.set("metadata.dateModified", Date.now());
