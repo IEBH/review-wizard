a5d3b162f1e3c10fd00513d7efbb22056469f58c
Octian <115122605+Octian@users.noreply.github.com>
2024-05-30 12:16:34 +1000

    Update ViewMethodResearchPlan.vue

--- src/project-templates/methods-wizard/ViewMethodResearchPlan.vue
+++ src/project-templates/methods-wizard/ViewMethodResearchPlan.vue
@@ -29,15 +29,15 @@ export default {
 			) {
 				da = [...this.$tera.state.author, ...this.$tera.state.acknowledgements];
 			}
-			this.checkUnion(da);
+			
 			/**
 			 * return this.authorsArr?.map(el => {
 				return { label: el };
 			});
 			 */
-			return da?.map(el => {
+			return this.checkUnion(da?.map(el => {
 				return { label: el };
-			});
+			}));;
 		}
 	},
 	methods: {
