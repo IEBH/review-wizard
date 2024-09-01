c15fd7e1dbe7453e6bf8985bcedc18b6b456ebd5
s5333384 <140589649+s5333384@users.noreply.github.com>
2024-09-02 09:29:55 +1000

    Bug Update

---
+++ src/project-templates/methods-wizard/ViewMethodReplicantResults.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<h1>Results</h1>
+		<InputEditorMultiline
+			question=""
+			:placeholder="placeholder"
+			v-model="$tera.state.metaReplicant.file"
+		/>
+	</div>
+</template>
+<script>
+import InputEditorMultiline from "@/components/InputEditorMultiline.vue";
+
+export default {
+	components: {
+		InputEditorMultiline
+	},
+	data() {
+		return {
+			placeholder:'Replicant Results'
+		};
+	}
+};
+</script>
